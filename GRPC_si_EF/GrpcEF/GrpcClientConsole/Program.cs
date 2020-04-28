eddd89667fdbc8dfaa57a22785e7a98e3faa6cb1
alexmocanu07 <46425351+alexmocanu07@users.noreply.github.com>
2020-04-28 16:01:18 +0300

    GRPC_EF Lab

---
+++ GRPC_si_EF/GrpcEF/GrpcClientConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using System;
+
+namespace GrpcClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+
+        }
+    }
+}
---
+++ GRPC_si_EF/GrpcEF/GrpcService/Services/PostCommentService.cs
@@ -0,0 +1,50 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PostComment.APIStatic;
+using PostComment;
+
+namespace GrpcService.Services
+{
+    public class PostCommentService : Protos.GrpcApi.GrpcApiBase
+    {
+        private Comment GetCommentFromModel(CommentModel model)
+        {
+            return new Comment()
+            {
+                CommentId = model.CommentId,
+                Text = model.Text,
+                PostPostId = model.PostPostId
+            };
+        }
+        private Post GetPostFromPostModel(PostModel model)
+        {
+            var comments = model.Comments;
+
+            var p = new Post()
+            {
+                PostId = model.PostId,
+                Domain = model.Domain,
+                Description = model.Descripion,
+                Date = DateTime.Parse(model.Date.ToString())
+            };
+
+            p.Comments.AddRange(model.Comments.Select(GetCommentFromModel));
+
+            return p;
+            
+        }
+        public override Task<BoolResultMessage> AddPost(PostModel newPost, ServerCallContext context)
+        {
+            bool result = API.AddPost(GetPostFromPostModel(newPost));
+
+            return Task.FromResult(new BoolResultMessage()
+            {
+                Result = result
+            });
+        }
+    }
+}
