00d123763e1d652562b1c63e9f626b27294a42c4
alexmocanu07 <46425351+alexmocanu07@users.noreply.github.com>
2020-05-05 09:53:44 +0300

    RazorPages Lab

---
+++ Asp_Mocanu_Alexandru_Rp/Asp_Mocanu_Alexandru_Rp/Data/Asp_Mocanu_Alexandru_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mocanu_Alexandru_Rp.Models;
+
+namespace Asp_Mocanu_Alexandru_Rp.Data
+{
+    public class Asp_Mocanu_Alexandru_RpContext : DbContext
+    {
+        public Asp_Mocanu_Alexandru_RpContext (DbContextOptions<Asp_Mocanu_Alexandru_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Mocanu_Alexandru_Rp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Asp_Mocanu_Alexandru_Rp/Asp_Mocanu_Alexandru_Rp/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mocanu_Alexandru_Rp.Data;
+using Asp_Mocanu_Alexandru_Rp.Models;
+
+namespace Asp_Mocanu_Alexandru_Rp.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Asp_Mocanu_Alexandru_Rp.Data.Asp_Mocanu_Alexandru_RpContext _context;
+
+        public DetailsModel(Asp_Mocanu_Alexandru_Rp.Data.Asp_Mocanu_Alexandru_RpContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
